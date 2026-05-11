c3fb34f97445a2c5fd30da64668d803ab5eec6c7
junia <junia@kali.junia>
2026-05-11 14:30:17 -0400

    jf

---
+++ target/debug/deps/bill_manager-790a8fcd09bfff6f.d
@@ -0,0 +1,5 @@
+/home/junia/code/stellar/bill_manager/bill_manager/target/debug/deps/bill_manager-790a8fcd09bfff6f.d: src/main.rs
+
+/home/junia/code/stellar/bill_manager/bill_manager/target/debug/deps/libbill_manager-790a8fcd09bfff6f.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bill_manager-82685997dee716ba.d
@@ -0,0 +1,5 @@
+/home/junia/code/stellar/bill_manager/bill_manager/target/debug/deps/bill_manager-82685997dee716ba.d: src/main.rs
+
+/home/junia/code/stellar/bill_manager/bill_manager/target/debug/deps/bill_manager-82685997dee716ba: src/main.rs
+
+src/main.rs:
